0fd763cab364ef39588d939277e7f687fbdfb8c0
yousefha2002 <yousefha2029@gmail.com>
2025-05-10 09:50:34 +0300

    add li for lists

--- components/ui/Label.tsx
+++ components/ui/Label.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 
 export default function Label({ name, id }: { name: string; id: number }) {
     return (
-        <Link
-            href={{
-                pathname: '/articles',
-                query: { category: id },
-            }}
-            className="py-1 px-3 rounded-full bg-primary text-white"
-        >
-            {name}
-        </Link>
+        <li>
+            <Link
+                href={{
+                    pathname: '/articles',
+                    query: { category: id },
+                }}
+                className="py-1 px-3 rounded-full bg-primary text-white"
+            >
+                {name}
+            </Link>
+        </li>
     );
 }
