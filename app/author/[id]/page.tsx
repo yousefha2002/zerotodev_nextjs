a560572ed8c77e850195e40c3317046cb4137037
yousefha2002 <yousefha2029@gmail.com>
2025-05-15 10:08:08 +0300

    edit gap - notFound()

--- app/author/[id]/page.tsx
+++ app/author/[id]/page.tsx
@@ -10,7 +10,7 @@ import React from 'react'
 export default async function page({params}:{params:Promise<{id:string}>}) {
     const {id} = await params ;
     const user = await getUserProfilePublic(+id)
-    if(!user)notFound
+    if(!user)notFound()
     else
     return (
         <Container className='py-6'>
