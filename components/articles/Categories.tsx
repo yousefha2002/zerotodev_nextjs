505c0c6f95c3eee779b1b7580225b52f984b6c03
moh7amed2002 <moha7medheshamabohani@gmail.com>
2025-06-04 10:55:27 +0300

    add all button

--- components/articles/Categories.tsx
+++ components/articles/Categories.tsx
@@ -2,6 +2,7 @@ import { CiShoppingTag } from "react-icons/ci";
 import SidebarCard from "../ui/sidebar/SidebarCard";
 import Label from "../ui/Label";
 import { Category } from "@/types/Category";
+import Link from "next/link";
 
 type CategoriesProps = {
     categories: Category[];
@@ -11,6 +12,14 @@ const Categories = ({ categories }: CategoriesProps) => {
     return (
         <SidebarCard title="التصنيفات المميزة" icon={<CiShoppingTag size={24} />}>
             <ul className="flex flex-wrap gap-2 py-6">
+                <Link
+                href={{
+                    pathname: '/articles',
+                    }}
+                    className="py-1 px-3 rounded-full bg-primary text-white"
+                >
+                    الكل
+                </Link>
                 {categories.map((category) => (
                     <Label {...category} key={category.id} />
                 ))}
